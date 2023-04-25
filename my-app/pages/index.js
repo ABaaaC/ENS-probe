9f1d9de2b11ba192e01f72c8187a18498875116e
Stanislav <rus.abaaac@gmail.com>
2023-04-26 01:05:59 +0300

    Versioning untracked files

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import React, { useEffect, useRef, useState } from "react"
 
-import { utils, providers } from "ethers"
+import { providers } from "ethers"
 import Web3Modal from "web3modal"
 
 export default function Home() {
